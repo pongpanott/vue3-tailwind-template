0dae4f31058c372ab177b3c48232185f79d3f387
pongpanott <fhyptr@gmail.com>
2022-05-08 16:43:44 +0700

    initital commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p class="text-red-400 text-2xl">hellp world</p>
+    <button class="rounded-lg bg-teal-100 text-teal-500 p-4">tailwind</button>
+    <router-view />
+  </div>
+</template>
